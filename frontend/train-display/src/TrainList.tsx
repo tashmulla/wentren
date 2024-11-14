6e310290280540e8e99d3bce78e71f923ce25109
Tash Mulla <natashashafqat@gmail.com>
2024-11-14 23:07:53 +0000

    Update frontend with new RDM data and update look & feel

--- frontend/train-display/src/TrainList.tsx
+++ frontend/train-display/src/TrainList.tsx
@@ -24,54 +24,85 @@ const TrainList: React.FC = () => {
 
 	const loadTrainData = () => {
 		fetchTrainDetails()
-		  .then((data) => {
-			if (data.length) {
-			  const updatedTrains = data.map((train: Train) => {
-				const trainTime = new Date(train.startTime);
-				const timeDiff = Math.floor((trainTime.getTime() - currentTime.getTime()) / 60000);
-				return { ...train, timeUntil: timeDiff };
-			  });
-			  setTrains(updatedTrains);
-			}
-		  })
-		  .catch((error) => setError(error.message));
+			.then((data) => {
+				if (data.length) {
+					const updatedTrains = data.map((train: Train) => {
+						const timeToUse = train.estimatedTime !== 'On time' ? train.estimatedTime : train.scheduledTime;
+						const trainTime = new Date();
+						const [hours, minutes] = timeToUse.split(':').map(Number);
+						trainTime.setHours(hours, minutes, 0, 0);
+
+						const timeDiff = Math.floor((trainTime.getTime() - currentTime.getTime()) / 60000);
+						return { ...train, timeUntil: isNaN(timeDiff) ? null : timeDiff };
+					});
+					setTrains(updatedTrains);
+				}
+			})
+			.catch((error) => setError(error.message));
 	};
 
 	useEffect(() => {
-	loadTrainData();
-
-	const dataInterval = setInterval(() => {
 		loadTrainData();
-	}, 60000);
 
-	return () => clearInterval(dataInterval);
+		const dataInterval = setInterval(() => {
+			loadTrainData();
+		}, 60000);
+
+		return () => clearInterval(dataInterval);
 	}, [currentTime]);
 
 	const formatTime = (dateTime: string) => {
-	const date = new Date(dateTime);
-	return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+		const date = new Date(dateTime);
+		return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+	};
+
+	const renderTrainDetails = (train: Train) => {
+		const isDelayed = train.estimatedTime !== 'On time';
+		return (
+			<div className="train-details">
+				<span className={`train-status ${isDelayed ? 'delayed' : 'on-time'}`}>
+					{isDelayed ? 'Delayed' : 'On Time'}
+				</span>
+				{' / '}
+				<span className="train-scheduled-time">{train.scheduledTime}</span>
+				{isDelayed && (
+					<>
+						{' / '}
+						<span className="train-estimated-time">{train.estimatedTime}</span>
+					</>
+				)}
+				{' / '}
+				<span className="train-time-until">{train.timeUntil} mins away</span>
+			</div>
+		);
 	};
 
 	return (
-	<>
-	  <div className="current-time">
-		{currentTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' })}
-	  </div>
-	  {error && <div>Error: {error}</div>}
-	  {!error && !trains.length && <div>Loading...</div>}
-	  {trains.length > 0 && (
-		<ul>
-		  {trains.map((train, index) => (
-			<li key={index}>
-			  <div className="train-time">{formatTime(train.startTime)}</div>
-			  <div className="train-info">
-				{train.timeUntil} mins away - {train.numberOfStops} stops - {train.duration} mins journey - <span className="train-destination">{train.destination}</span>
-			  </div>
-			</li>
-		  ))}
-		</ul>
-	  )}
-	</>
+		<div className="train-list-container">
+			<div className="current-time">
+				{currentTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' })}
+			</div>
+			{error && <div>Error: {error}</div>}
+			{!error && !trains.length && <div>Loading...</div>}
+			{trains.length > 0 && (
+				<ul>
+					{trains.map((train, index) => (
+						<li key={index} className="train-item">
+							<div className="train-info">
+								<div className="train-header">
+									<span className="train-destination">
+										{train.destination} ({train.origin})
+									</span>
+									<span className="train-stops"> - {train.numberOfStops} stops</span>
+								</div>
+								{renderTrainDetails(train)}
+							</div>
+							<hr className="train-divider" />
+						</li>
+					))}
+				</ul>
+			)}
+		</div>
 	);
 };
 
